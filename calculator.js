b9e0c707d55b541fdc78a7d8ac87c1582c8b91c8
Max Elbirt <maxelbirt@gmail.com>
2022-11-20 01:58:18 +0200

    a lot of improvements to code and layout. made everything more efficient and designed different display mode for the two main media queries

--- calculator.js
+++ calculator.js
@@ -1,127 +1,87 @@
-let buttons = document.getElementsByTagName("button");
+let buttons = document.querySelectorAll("button");
 
-let num1 = null;
-let num2 = null;
-let num_digits = null;
+let num1 = "";
+let num2 = "";
 let operator = null;
 
 let digits = "0123456789"
 let operators = "+-*/"
 
-function add(num1, num2){
-    return Number(num1) + Number(num2);
-}
-
-function subtract(num1, num2){
-    return Number(num1) - Number(num2);
-}
-
-function multiply(num1, num2){
-    return Number(num1) * Number(num2);
-}
-
-function divide(num1, num2){
-    return Number(num1) / Number(num2);
-}
-
-for (let button of buttons) {
-    button.addEventListener("click", () =>{
-        // First Number
-        if (digits.includes(button.getAttribute("value")) && num1 === null && operator === null & num2 === null){
-            num1 = button.getAttribute("value");
-            document.getElementById("display").setAttribute("value", num1);
-            console.log(operator)
-            console.log(num1);
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // if only numbers have been hit so far -----------> num1 +=
+        if (digits.includes(button.value) && operator === null) {
+            num1 += button.value;
+            document.querySelector("input#display").value = Number(num1);        
         }
-        else if (operators.includes(button.getAttribute("value")) && num1 !== null && num2 === null){
-            operator = button.getAttribute("value");
-            document.getElementById("display").setAttribute("value", num1 + operator);
-            console.log(operator)
-            console.log(num1);
-        }
-        else if (!operators.includes(button.getAttribute("value")) && num1 !== null && operator === null && num2 === null){
-            num_digits = button.getAttribute("value");
-            num1 = num1 + num_digits;
-            document.getElementById("display").setAttribute("value", num1);
-            operator = null;
-            console.log(operator);
-            console.log(num1);
-        } 
-
-        // Second Number
-        else if (digits.includes(button.getAttribute("value")) && num1 !== null && operator !== null & num2 === null){
-            num2 = button.getAttribute("value");
-            document.getElementById("display").setAttribute("value", num1 + operator + num2 );
-            console.log("num2: " + num2);
-            console.log("num digits is: " + num_digits);
+        
+        //if an operator key is hit for the first time
+        else if (operators.includes(button.value) && num1 !== "" && num2 === "") {
+            operator = button.value;
+            document.querySelector("input#display").value = Number(num1) + operator;
         }
 
-        else if (digits.includes(button.getAttribute("value")) && num2 !== null){
-            num_digits = button.getAttribute("value");
-            num2 = num2 + num_digits;
-            document.getElementById("display").setAttribute("value", num1 + operator + num2);
-            console.log(operator);
-            console.log(num1);
-            console.log(num_digits);
-            console.log(num2);
+        //if a number is hit when an operator has been chosen ----> num2 +=
+        else if (digits.includes(button.value) && operator !== null) {
+            num2 += button.value;
+            document.querySelector("input#display").value = num1 + operator + Number(num2);
         }
 
-        else if (operators.includes(button.getAttribute("value")) && num2 !== null){
+        // if "=" key or an operator is hit after num2 has been filled --------> result is calculated
+        else if (button.value === "=" || (operators.includes(button.value) && num2 !== "")) {
+            num1 = eval(num1 + operator + num2);
+            num2 = "";
+            document.querySelector("input#display").value = Number(num1);
+        }
 
-            // Math Function Calls
-            if (operator === "+") {
-                document.getElementById("display").setAttribute("value", add(num1, num2));
-                num1 = add(num1, num2);
-            } 
-            else if (operator === "-") {
-                document.getElementById("display").setAttribute("value", subtract(num1, num2));
-                num1 = subtract(num1, num2);
-            } 
-            else if (operator === "*") {
-                document.getElementById("display").setAttribute("value", multiply(num1, num2));
-                num1 = multiply(num1, num2);
-            } 
-            else if (operator === "/") {
-                document.getElementById("display").setAttribute("value", divide(num1, num2));
-                num1 = divide(num1, num2);
+        //if decimal point key is hit ---------> number turns into float
+        else if (button.value === "decimal") {
+            if (operator === null) {
+                num1 += ".";
             }
 
-            operator = button.getAttribute("value")
-            num2 = null
-
+            else if (operator !== null && num2 !== ""){
+                num2 += ".";
+            }
         }
 
-        // Math Function Calls
-        else if (button.getAttribute("Value") === "=" && operator === "+") {
-            document.getElementById("display").setAttribute("value", add(num1, num2));
-        } 
-        else if (button.getAttribute("Value") === "=" && operator === "-") {
-            document.getElementById("display").setAttribute("value", subtract(num1, num2));
-        } 
-        else if (button.getAttribute("Value") === "=" && operator === "*") {
-            document.getElementById("display").setAttribute("value", multiply(num1, num2));
-        } 
-        else if (button.getAttribute("Value") === "=" && operator === "/") {
-            document.getElementById("display").setAttribute("value", divide(num1, num2));
-        }
-
-        // Utilities
-        else if (button.getAttribute("Value") === "clear"){
-            num1 = null;
-            num2 = null;
+        // if "clear" key is hit
+        else if (button.value === "clear") {
+            num1 = "";
+            num2 = "";
             operator = null;
-            document.getElementById("display").setAttribute("value", 0);
-            console.log(operator)
-            console.log(num1);
-            console.log(num2);
+            document.querySelector("input#display").value = 0;
         }
     })
-}
+})
 
 
 
 
 
+
+
+
+
+
+
+
+// function add(num1, num2){
+    //     return Number(num1) + Number(num2);
+    // }
+    
+    // function subtract(num1, num2){
+    //     return Number(num1) - Number(num2);
+    // }
+    
+    // function multiply(num1, num2){
+    //     return Number(num1) * Number(num2);
+    // }
+    
+    // function divide(num1, num2){
+    //     return Number(num1) / Number(num2);
+    // }
+    
 
 
 
@@ -153,3 +113,96 @@ for (let button of buttons) {
 // }
 
 
+// for (let button of buttons) {
+    //     button.addEventListener("click", () =>{
+    //         // First Number
+    //         if (digits.includes(button.getAttribute("value")) && num1 === null && operator === null & num2 === null){
+    //             num1 = button.getAttribute("value");
+    //             document.getElementById("display").setAttribute("value", num1);
+    //             console.log(operator)
+    //             console.log(num1);
+    //         }
+    //         else if (operators.includes(button.getAttribute("value")) && num1 !== null && num2 === null){
+    //             operator = button.getAttribute("value");
+    //             document.getElementById("display").setAttribute("value", num1 + operator);
+    //             console.log(operator)
+    //             console.log(num1);
+    //         }
+    //         else if (!operators.includes(button.getAttribute("value")) && num1 !== null && operator === null && num2 === null){
+    //             num_digits = button.getAttribute("value");
+    //             num1 = num1 + num_digits;
+    //             document.getElementById("display").setAttribute("value", num1);
+    //             operator = null;
+    //             console.log(operator);
+    //             console.log(num1);
+    //         } 
+    
+    //         // Second Number
+    //         else if (digits.includes(button.getAttribute("value")) && num1 !== null && operator !== null & num2 === null){
+    //             num2 = button.getAttribute("value");
+    //             document.getElementById("display").setAttribute("value", num1 + operator + num2 );
+    //             console.log("num2: " + num2);
+    //             console.log("num digits is: " + num_digits);
+    //         }
+    
+    //         else if (digits.includes(button.getAttribute("value")) && num2 !== null){
+    //             num_digits = button.getAttribute("value");
+    //             num2 = num2 + num_digits;
+    //             document.getElementById("display").setAttribute("value", num1 + operator + num2);
+    //             console.log(operator);
+    //             console.log(num1);
+    //             console.log(num_digits);
+    //             console.log(num2);
+    //         }
+    
+    //         else if (operators.includes(button.getAttribute("value")) && num2 !== null){
+    
+    //             // Math Function Calls
+    //             if (operator === "+") {
+    //                 document.getElementById("display").setAttribute("value", add(num1, num2));
+    //                 num1 = add(num1, num2);
+    //             } 
+    //             else if (operator === "-") {
+    //                 document.getElementById("display").setAttribute("value", subtract(num1, num2));
+    //                 num1 = subtract(num1, num2);
+    //             } 
+    //             else if (operator === "*") {
+    //                 document.getElementById("display").setAttribute("value", multiply(num1, num2));
+    //                 num1 = multiply(num1, num2);
+    //             } 
+    //             else if (operator === "/") {
+    //                 document.getElementById("display").setAttribute("value", divide(num1, num2));
+    //                 num1 = divide(num1, num2);
+    //             }
+    
+    //             operator = button.getAttribute("value")
+    //             num2 = null
+    
+    //         }
+    
+    //         // Math Function Calls
+    //         else if (button.getAttribute("Value") === "=" && operator === "+") {
+    //             document.getElementById("display").setAttribute("value", add(num1, num2));
+    //         } 
+    //         else if (button.getAttribute("Value") === "=" && operator === "-") {
+    //             document.getElementById("display").setAttribute("value", subtract(num1, num2));
+    //         } 
+    //         else if (button.getAttribute("Value") === "=" && operator === "*") {
+    //             document.getElementById("display").setAttribute("value", multiply(num1, num2));
+    //         } 
+    //         else if (button.getAttribute("Value") === "=" && operator === "/") {
+    //             document.getElementById("display").setAttribute("value", divide(num1, num2));
+    //         }
+    
+    //         // Utilities
+    //         else if (button.getAttribute("Value") === "clear"){
+    //             num1 = null;
+    //             num2 = null;
+    //             operator = null;
+    //             document.getElementById("display").setAttribute("value", 0);
+    //             console.log(operator)
+    //             console.log(num1);
+    //             console.log(num2);
+    //         }
+    //     })
+    // }
